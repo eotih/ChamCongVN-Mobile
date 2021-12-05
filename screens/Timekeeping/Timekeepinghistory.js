1f2e31bab7bb6442e14bf50f4c58ce64b074d749
DESKTOP-JL5NQM2\baoda <dqbtt2000@gmail.com>
2021-12-05 16:45:18 +0700

    New

--- screens/Timekeeping/Timekeepinghistory.js
+++ screens/Timekeeping/Timekeepinghistory.js
@@ -1,31 +1,80 @@
-import React from "react";
-import { ScrollView, StyleSheet, Dimensions, View, Image, TouchableOpacity } from "react-native";
-import { GetAllTimeKeepingByEmployeeID } from "../../functions/TimeKeeper";
+import React, { useState, useEffect, useRef } from 'react';
+import { ScrollView, StyleSheet, Dimensions, View, Image, TouchableOpacity, Button, TextInput, } from "react-native";
+import data from "../../constants/datatest";
 import { Block, Text, Button as GaButton, theme } from "galio-framework";
-import CardHistory from "../../components/CardHistory"
+import CardHistory from "../../components/CardHistory";
+import { Input } from "../../components";
+import moment from "moment";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { IconButton, Colors } from 'react-native-paper';
 
-class TimekeepingHistory extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      Time: [],
-    }
+
+function TimekeepingHistory() {
+  const [date, setDate] = useState(new Date());
+  const [day, setDay] = useState();
+  const [dataCICO, setdataCICO] = useState(data);
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    setShow(false)
+  }, [show]);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    const selectedDay = moment(currentDate).format("LL");
+    setDay(selectedDay);
+    filterData(selectedDay);
+    setDate(currentDate);
+  };
+  const filterData = (condition) => {
+    const result = data.filter(res => moment(res.checkin.CreatedDate).format("LL") == condition);
+    setdataCICO(result);
   }
-  componentDidMount() {
-    GetAllTimeKeepingByEmployeeID(1).then(res => {
-      this.setState({
-        Time: res
-      })
-    })
-  }
-  render() {
-    return (<ScrollView>
-      {this.state.Time.map(data => <CardHistory
-        key={data.$id}
-        data={data}
-      />)}
-    </ScrollView>
-    );
-  }
+
+  return (
+    <>
+      <View style={styles.filter}>
+         <TextInput size={16} color="#32325D" style={styles.input}
+          placeholder="Select Date"
+          value={day}
+        >
+        </TextInput>
+        <IconButton style={styles.icon}
+          icon="calendar"
+          color={Colors.red500}
+          size={32}
+          onPress={() => setShow(true)}
+        />
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode='date'
+            is24Hour={false}
+            display="default"
+            onChange={onChange}
+          />
+        )}
+      </View>
+      <ScrollView>
+        {dataCICO.map(data => <CardHistory
+          key={data.id}
+          data={data}
+        />)}
+      </ScrollView>
+    </>
+  );
 }
+const { width, height } = Dimensions.get('window');
+const styles = StyleSheet.create({
+  filter: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderWidth: 1,
+    marginHorizontal: theme.SIZES.BASE,
+    borderColor: "blue",
+  },
+  input: {
+    marginLeft: 10,
+  },
+})
 export default TimekeepingHistory;
